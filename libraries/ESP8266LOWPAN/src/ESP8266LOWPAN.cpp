f1d20b6496dc2b8e0a7361df7310177a5c18cc84
Vinicius Marques <vini.type@gmail.com>
2017-06-05 16:11:11 -0300

    Some enhancements to have CoAP native support

---
+++ libraries/ESP8266LOWPAN/src/ESP8266LOWPAN.cpp
@@ -0,0 +1,27 @@
+/*
+  ESP8266LoWPAN.cpp - A 6LowPAN implementation based on RFC 4944, RFC 6282 and RFC 7400.
+  Supports only one simultaneous client, knows how to handle GET and POST.
+
+  Copyright (c) 2017 Vinicius de F. Marques. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  Modified 8 May 2015 by Hristo Gochkov (proper post and file upload handling)
+*/
+
+#include "ESP8266LOWPAN.h"
+
+int ESP8266LOWPAN::test(){
+  return 1;
+}
